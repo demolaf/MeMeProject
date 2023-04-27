b42ded6666f4f07ff9aa3e3207ac39229be56ec1
Ademola Fadumo <demolafadumo@gmail.com>
2023-04-27 17:19:16 +0100

    completed meme 2.0

--- MeMe Project/Controllers/Collection/CollectionViewController.swift
+++ MeMe Project/Controllers/Collection/CollectionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     var memes: [Meme]! {
         let object = UIApplication.shared.delegate
         let appDelegate = object as! AppDelegate
@@ -18,6 +20,13 @@ class CollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let space:CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
     
     override func viewDidAppear(_ animated: Bool) {
